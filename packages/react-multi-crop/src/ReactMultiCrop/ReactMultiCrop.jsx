c7e12290ca77ccec017ded6c2331f0c25c557254
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-11-28 14:10:09 +0700

    feat: new arch
    * Add new arch
    * Fix some bugs
    * Add New Key
    * Add auto transpile

--- packages/react-multi-crop/src/ReactMultiCrop/ReactMultiCrop.jsx
+++ packages/react-multi-crop/src/ReactMultiCrop/ReactMultiCrop.jsx
@@ -1,136 +1,161 @@
-import React, { Component } from 'react'
-import { fabric } from 'fabric'
-import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { fabric } from "fabric";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import PropTypes from "prop-types";
 
 class ReactMultiCrop extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       canvas: null,
-      initial: true
-    }
+      initial: true,
+    };
 
-    this.color = props.cropBackgroundColor
-    this.opacity = props.cropBackgroundOpacity
-    this.strokeColor = props.cropOutlineColor
-    this.strokeWidth = props.cropOutlineWidth
-    this.strokeDashArray = [5, 5]
+    this.color = props.cropBackgroundColor;
+    this.opacity = props.cropBackgroundOpacity;
+    this.strokeColor = props.cropOutlineColor;
+    this.strokeWidth = props.cropOutlineWidth;
+    this.strokeDashArray = [5, 5];
 
-    this.keyboardHandler = this.keyboardHandler.bind(this)
-    this.addNew = this.addNew.bind(this)
-    this.deleteShapes = this.deleteShapes.bind(this)
-    this.multiSelect = this.multiSelect.bind(this)
-    this.discardActiveObject = this.discardActiveObject.bind(this)
+    this.keyboardHandler = this.keyboardHandler.bind(this);
+    this.addNew = this.addNew.bind(this);
+    this.deleteShapes = this.deleteShapes.bind(this);
+    this.multiSelect = this.multiSelect.bind(this);
+    this.discardActiveObject = this.discardActiveObject.bind(this);
   }
 
   componentDidMount() {
     const { canvas } = this.state;
     if (!canvas) {
-      this.initialCanvas()
+      this.initialCanvas();
     }
   }
 
   componentDidUpdate() {
-    this.changeImage()
+    this.changeImage();
   }
 
   changeImage() {
     const { canvas } = this.state;
+    if (!canvas) {
+      return;
+    }
     if (canvas.backgroundImage) {
       return;
     }
-    const { record, image } = this.props
-    let setImage = this.loadImage.bind(this)
-    if (typeof record === 'object' && record.image) {
-      fabric.Image.fromURL(record.image, setImage)
-    } else if (typeof image === 'string') {
-      fabric.Image.fromURL(image, setImage)
+    const { record, image } = this.props;
+    let setImage = this.loadImage.bind(this);
+    if (typeof record === "object" && record.image) {
+      fabric.Image.fromURL(record.image, setImage);
+    } else if (typeof image === "string") {
+      fabric.Image.fromURL(image, setImage);
     }
   }
 
   loadImage(img) {
-    let { initial, canvas } = this.state
+    let { initial, canvas } = this.state;
+    if (!canvas) {
+      return;
+    }
     canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
       scaleX: canvas.width / img.width,
-      scaleY: canvas.height / img.height
-    })
-    if (typeof initial === 'boolean' && initial) {
-      this.setState({ initial: false }, this.initialObjects.bind(this))
+      scaleY: canvas.height / img.height,
+    });
+    if (typeof initial === "boolean" && initial) {
+      this.setState({ initial: false }, this.initialObjects.bind(this));
     }
   }
 
   initialImage() {
-    const { record, image } = this.props
-    let loadImageNow = this.loadImage.bind(this)
-    if (typeof record === 'object' && record.image) {
-      fabric.Image.fromURL(record.image, loadImageNow)
-    } else if (typeof image === 'string') {
-      fabric.Image.fromURL(image, loadImageNow)
+    const { record, image } = this.props;
+    let loadImageNow = this.loadImage.bind(this);
+    if (typeof record === "object" && record.image) {
+      fabric.Image.fromURL(record.image, loadImageNow);
+    } else if (typeof image === "string") {
+      fabric.Image.fromURL(image, loadImageNow);
     }
   }
 
   initialObjects() {
-    let { canvas } = this.state
-    const { record } = this.props
-    if (typeof record === 'object') {
-      let setOutput = this.setOutput.bind(this)
-      let setStateOf = this.setState.bind(this)
-      let inputObject = record.clippings
-      let createObject = this.createObject.bind(this)
-      if (Array.isArray(inputObject) && inputObject.length > 0 && typeof inputObject[0] === 'object') {
+    let { canvas } = this.state;
+    if (!canvas) {
+      return;
+    }
+    const { record } = this.props;
+    if (typeof record === "object" && !record) {
+      let setOutput = this.setOutput.bind(this);
+      let setStateOf = this.setState.bind(this);
+      let inputObject = record.clippings;
+      let createObject = this.createObject.bind(this);
+      if (
+        Array.isArray(inputObject) &&
+        inputObject.length > 0 &&
+        typeof inputObject[0] === "object"
+      ) {
         inputObject.forEach(function (coord) {
-          let rect = createObject(canvas, coord)
-          canvas.add(rect)
-        })
+          let rect = createObject(canvas, coord);
+          canvas.add(rect);
+        });
       }
-      canvas.renderAll()
-      setStateOf({ canvas }, setOutput)
+      canvas.renderAll();
+      setStateOf({ canvas }, setOutput);
     }
   }
 
   initialCanvas() {
-    let canvas = new fabric.Canvas(this.props.id)
-    canvas.uniScaleTransform = true
-    let doubleClickEvent = this.doubleClickEvent.bind(this)
-    let objectModifiedEvent = this.setOutput.bind(this)
-    canvas.on('mouse:dblclick', doubleClickEvent)
-    canvas.on('object:modified', objectModifiedEvent)
-    let initialImg = this.initialImage.bind(this)
-    this.setState({ canvas }, initialImg)
+    let canvas = new fabric.Canvas(this.props.id);
+    canvas.uniScaleTransform = true;
+    let doubleClickEvent = this.doubleClickEvent.bind(this);
+    let objectModifiedEvent = this.setOutput.bind(this);
+    canvas.on("mouse:dblclick", doubleClickEvent);
+    canvas.on("object:modified", objectModifiedEvent);
+    let initialImg = this.initialImage.bind(this);
+    this.setState({ canvas }, initialImg);
   }
 
   addNew() {
-    let { canvas } = this.state
-    let coor = {}
-    coor.id = null
-    coor.rect = { 'x1': 0, 'y1': 0, 'x2': 0.2, 'y2': 0.2 }
-    let rect = this.createObject(canvas, coor)
-    rect.lockRotation = true
-    canvas.add(rect)
-    canvas.renderAll()
-    this.setState({ canvas }, this.setOutput)
+    let { canvas } = this.state;
+    if (!canvas) {
+      return;
+    }
+    let coor = {};
+    coor.id = null;
+    coor.rect = { x1: 0, y1: 0, x2: 0.2, y2: 0.2 };
+    let rect = this.createObject(canvas, coor);
+    rect.lockRotation = true;
+    canvas.add(rect);
+    canvas.renderAll();
+    this.setState({ canvas }, this.setOutput);
   }
 
   doubleClickEvent(options) {
-    if (typeof options === 'object' && typeof options.target === 'object') {
-      let { canvas } = this.state
-      const left = options.target.left
-      const top = options.target.top
-      const width = options.target.width
-      const height = options.target.height
-      let attribute = {}
-      attribute.left = left + 5
-      attribute.top = top + 5
-      attribute.width = width * options.target.scaleX
-      attribute.height = height * options.target.scaleY
-      let rect = this.createObjectByAttribute(attribute)
-      rect.lockRotation = true
-      canvas.add(rect)
-      canvas.renderAll()
-      this.setState({ canvas }, this.setOutput)
+    let { canvas } = this.state;
+    if (!canvas) {
+      return;
+    }
+    if (
+      typeof options === "object" &&
+      typeof options.target === "object" &&
+      options.target
+    ) {
+      const left = options.target.left;
+      const top = options.target.top;
+      const width = options.target.width;
+      const height = options.target.height;
+      let attribute = {};
+      attribute.left = left + 50;
+      attribute.top = top + 50;
+      attribute.width = width * options.target.scaleX;
+      attribute.height = height * options.target.scaleY;
+      let rect = this.createObjectByAttribute(attribute);
+      rect.lockRotation = true;
+      canvas.add(rect);
+      canvas.renderAll();
+      this.setState({ canvas }, this.setOutput);
+    } else {
+      this.addNew();
     }
   }
 
@@ -145,72 +170,85 @@ class ReactMultiCrop extends Component {
       id: null,
       strokeDashArray: this.strokeDashArray,
       stroke: this.strokeColor,
-      strokeWidth: this.strokeWidth
-    })
+      strokeWidth: this.strokeWidth,
+    });
   }
 
   shapetoStructureData(element) {
-    let { canvas } = this.state
-    let coord = {}
-    coord.id = element.id
-    let x1 = element.left / canvas.width
-    let y1 = element.top / canvas.height
-    let x2 = (element.left + (element.width * element.scaleX)) / canvas.width
-    let y2 = (element.top + (element.height * element.scaleY)) / canvas.height
-    let rectangle = { 'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2 }
-    coord.rect = JSON.stringify(rectangle)
-    if (typeof canvas === 'object' && typeof canvas.backgroundImage === 'object') {
-      let imgWidth = canvas.backgroundImage.width
-      let imgHeight = canvas.backgroundImage.height
-      let x1Px = x1 * imgWidth
-      let x2Px = x2 * imgWidth
-      let y1Px = y1 * imgHeight
-      let y2Px = y2 * imgHeight
-      let rectanglePx = { 'x1': x1Px, 'y1': y1Px, 'x2': x2Px, 'y2': y2Px }
-      coord.rectPx = JSON.stringify(rectanglePx)
+    let { canvas } = this.state;
+    if (!canvas) {
+      return;
     }
-    coord.deletedAt = '-1'
-    return coord
+    let coord = {};
+    coord.id = element.id;
+    let x1 = element.left / canvas.width;
+    let y1 = element.top / canvas.height;
+    let x2 = (element.left + element.width * element.scaleX) / canvas.width;
+    let y2 = (element.top + element.height * element.scaleY) / canvas.height;
+    let rectangle = { x1: x1, y1: y1, x2: x2, y2: y2 };
+    coord.rect = JSON.stringify(rectangle);
+    if (
+      typeof canvas === "object" &&
+      typeof canvas.backgroundImage === "object" &&
+      canvas.backgroundImage
+    ) {
+      let imgWidth = canvas.backgroundImage.width;
+      let imgHeight = canvas.backgroundImage.height;
+      let x1Px = x1 * imgWidth;
+      let x2Px = x2 * imgWidth;
+      let y1Px = y1 * imgHeight;
+      let y2Px = y2 * imgHeight;
+      let rectanglePx = { x1: x1Px, y1: y1Px, x2: x2Px, y2: y2Px };
+      coord.rectPx = JSON.stringify(rectanglePx);
+    }
+    coord.deletedAt = "-1";
+    return coord;
   }
 
   deleteShapes() {
-    let { canvas } = this.state
+    let { canvas } = this.state;
     if (canvas) {
       canvas.getActiveObjects().forEach(function (element) {
-        canvas.remove(element)
-      })
-      this.setState({ canvas }, this.setOutput)
+        canvas.remove(element);
+      });
+      this.setState({ canvas }, this.setOutput);
     }
   }
 
   setOutput() {
-    const { canvas } = this.state
-    let shapeToStructureData = this.shapetoStructureData.bind(this)
-    let outputValue = []
-    let cropcoords = canvas.getObjects()
+    const { canvas } = this.state;
+    if (!canvas) {
+      return;
+    }
+    let shapeToStructureData = this.shapetoStructureData.bind(this);
+    let outputValue = [];
+    let cropcoords = canvas.getObjects();
     cropcoords.forEach(function (element) {
-      outputValue.push(shapeToStructureData(element))
-    })
+      outputValue.push(shapeToStructureData(element));
+    });
     // let stringOut = JSON.stringify(outputValue)
-    const { input } = this.props
+    const { input } = this.props;
     if (input) {
-      input.onChange(outputValue)
+      input.onChange(outputValue);
     }
   }
 
   createObject(canvas, coor) {
-    let rectangle
-    if (typeof coor.rect === 'string') {
-      rectangle = JSON.parse(coor.rect)
-    } else {
-      rectangle = coor.rect
+    if (!canvas) {
+      return;
     }
-    let left = canvas.width * rectangle.x1
-    let top = canvas.height * rectangle.y1
-    let right = canvas.width * rectangle.x2
-    let bottom = canvas.height * rectangle.y2
-    let width = right - left
-    let height = bottom - top
+    let rectangle;
+    if (typeof coor.rect === "string") {
+      rectangle = JSON.parse(coor.rect);
+    } else {
+      rectangle = coor.rect;
+    }
+    let left = canvas.width * rectangle.x1;
+    let top = canvas.height * rectangle.y1;
+    let right = canvas.width * rectangle.x2;
+    let bottom = canvas.height * rectangle.y2;
+    let width = right - left;
+    let height = bottom - top;
     return new fabric.Rect({
       left: left,
       top: top,
@@ -221,43 +259,45 @@ class ReactMultiCrop extends Component {
       id: coor.id,
       strokeDashArray: this.strokeDashArray,
       stroke: this.strokeColor,
-      strokeWidth: this.strokeWidth
-    })
+      strokeWidth: this.strokeWidth,
+    });
   }
 
   multiSelect() {
-    let { canvas } = this.state
+    let { canvas } = this.state;
     if (canvas) {
-      canvas.discardActiveObject()
+      canvas.discardActiveObject();
       let sel = new fabric.ActiveSelection(canvas.getObjects(), {
-        canvas: canvas
-      })
-      canvas.setActiveObject(sel)
-      canvas.requestRenderAll()
+        canvas: canvas,
+      });
+      canvas.setActiveObject(sel);
+      canvas.requestRenderAll();
     } else {
-      console.log("Canvas not defined")
+      console.log("Canvas not defined");
     }
   }
 
   discardActiveObject() {
-    let { canvas } = this.state
+    let { canvas } = this.state;
     if (canvas) {
-      canvas.discardActiveObject()
-      canvas.requestRenderAll()
+      canvas.discardActiveObject();
+      canvas.requestRenderAll();
     } else {
-      console.log("Canvas not defined")
+      console.log("Canvas not defined");
     }
   }
 
   keyboardHandler(event) {
     if (event.keyCode === 46) {
       // Handle Delete
-      this.deleteShapes()
+      this.deleteShapes();
+    } else if (event.ctrlKey && event.keyCode === 65) {
+      this.multiSelect();
     }
   }
 
   render() {
-    const { input, source, hideLabel, ...otherProps } = this.props
+    const { input, source, hideLabel, hideButton, ...otherProps } = this.props;
     const renderInputRedux = !!input;
     let valueForm;
     let nameForm = source;
@@ -269,80 +309,120 @@ class ReactMultiCrop extends Component {
 
     return (
       <div id="canvas-wrapper">
-        {!hideLabel && <div className="label">
-          {nameForm}
-        </div>}
+        {!hideLabel && <div className="label">{nameForm}</div>}
 
-        <Grid container
+        <Grid
+          container
           direction="row"
           justify="flex-start"
           alignItems="flex-start"
-          spacing={8}>
+        >
           <Grid item xs onKeyDown={this.keyboardHandler} tabIndex="0">
-            <canvas width="800" height="800" style={{ border: '1px solid #aaa' }}{...otherProps} />
+            <canvas
+              width="800"
+              height="800"
+              style={{ border: "0px solid #aaa" }}
+              {...otherProps}
+            />
           </Grid>
-          <Grid container item xs
-            direction="column"
-            justify="flex-start"
-            alignItems="flex-start"
-            spacing={8}>
-            <Grid item xs>
-              <Button variant="contained" id="addmore" color="primary" onClick={this.addNew} > Add More Shapes</Button>
+          {!hideButton && (
+            <Grid
+              container
+              item
+              xs
+              direction="column"
+              justify="flex-start"
+              alignItems="flex-start"
+            >
+              <Grid item xs>
+                <Button
+                  variant="contained"
+                  id="addmore"
+                  color="primary"
+                  onClick={this.addNew}
+                >
+                  {" "}
+                  Add More Shapes
+                </Button>
+              </Grid>
+              <Grid item xs>
+                <Button
+                  variant="contained"
+                  id="deleteselected"
+                  color="primary"
+                  onClick={this.deleteShapes}
+                >
+                  {" "}
+                  Delete Selected Object{" "}
+                </Button>
+              </Grid>
+              <Grid item xs>
+                <Button
+                  variant="contained"
+                  id="multiselect"
+                  color="primary"
+                  onClick={this.multiSelect}
+                >
+                  {" "}
+                  Select All{" "}
+                </Button>
+              </Grid>
+              <Grid item xs>
+                <Button
+                  variant="contained"
+                  id="discard"
+                  color="primary"
+                  onClick={this.discardActiveObject}
+                >
+                  {" "}
+                  Discard Selection
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item xs>
-              <Button variant="contained" id="deleteselected" color="primary" onClick={this.deleteShapes}> Delete Selected Object </Button>
-            </Grid>
-            <Grid item xs>
-              <Button variant="contained" id="multiselect" color="primary" onClick={this.multiSelect}> Select All </Button>
-            </Grid>
-            <Grid item xs >
-              <Button variant="contained" id="discard" color="primary" onClick={this.discardActiveObject}> Discard Selection</Button>
-            </Grid>
-          </Grid>
+          )}
           {renderInputRedux && <input type="hidden" value={valueForm} />}
         </Grid>
       </div>
-    )
+    );
   }
 }
 
 ReactMultiCrop.defaultProps = {
-  id: 'canvas',
+  id: "canvas",
   input: null,
-  source: 'react-crop-form',
-  hideLabel: false,
+  source: "react-crop-form",
+  hideLabel: true,
+  hideButton: true,
   record: {
     image: null,
-    clippings: []
+    clippings: [],
   },
   image: null,
-  cropBackgroundColor: 'yellow',
+  cropBackgroundColor: "yellow",
   cropBackgroundOpacity: 0.5,
-  cropOutlineColor: 'yellow',
-  cropOutlineWidth: 5
-}
+  cropOutlineColor: "yellow",
+  cropOutlineWidth: 5,
+};
 
 ReactMultiCrop.propTypes = {
   id: PropTypes.string,
   source: PropTypes.string,
   input: PropTypes.shape({
-    value: PropTypes.oneOfType([
-      PropTypes.object,
-      PropTypes.string
-    ]),
+    value: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
     name: PropTypes.string,
-    onChange: PropTypes.func
+    onChange: PropTypes.func,
   }),
   hideLabel: PropTypes.bool,
+  hideButton: PropTypes.bool,
   record: PropTypes.shape({
     image: PropTypes.string,
-    clippings: PropTypes.array
+    clippings: PropTypes.array,
   }),
   image: PropTypes.string,
   cropBackgroundColor: PropTypes.string,
-  cropBackgroundColor: PropTypes.number,
+  cropBackgroundOpacity: PropTypes.number,
   cropOutlineColor: PropTypes.string,
-  cropOutlineWidth: PropTypes.number
-}
+  cropOutlineWidth: PropTypes.number,
+};
 
-export default ReactMultiCrop
+export default ReactMultiCrop;
