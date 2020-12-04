a6433bb694c576ef3fdd4c094d6ec64e4836fa8b
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-12-04 19:09:04 +0700

    fix: fix zoom and drag

--- packages/react-multi-crop/src/ReactMultiCrop/ReactMultiCrop.jsx
+++ packages/react-multi-crop/src/ReactMultiCrop/ReactMultiCrop.jsx
@@ -131,25 +131,9 @@ class ReactMultiCrop extends Component {
     zoom *= 0.999 ** delta;
     if (zoom > 20) zoom = 20;
     if (zoom < 0.01) zoom = 0.01;
-    canvas.zoomToPoint({ x: options.e.offsetX, y: options.e.offsetY }, zoom);
+    canvas.setZoom(zoom);
     options.e.preventDefault();
     options.e.stopPropagation();
-    var vpt = canvas.viewportTransform;
-    if (zoom < 400 / 1000) {
-      vpt[4] = 200 - (1000 * zoom) / 2;
-      vpt[5] = 200 - (1000 * zoom) / 2;
-    } else {
-      if (vpt[4] >= 0) {
-        vpt[4] = 0;
-      } else if (vpt[4] < canvas.getWidth() - 1000 * zoom) {
-        vpt[4] = canvas.getWidth() - 1000 * zoom;
-      }
-      if (vpt[5] >= 0) {
-        vpt[5] = 0;
-      } else if (vpt[5] < canvas.getHeight() - 1000 * zoom) {
-        vpt[5] = canvas.getHeight() - 1000 * zoom;
-      }
-    }
   }
 
   initialCanvas() {
@@ -165,6 +149,34 @@ class ReactMultiCrop extends Component {
     canvas.on("mouse:dblclick", doubleClickEvent);
     canvas.on("object:modified", objectModifiedEvent);
     canvas.on("mouse:wheel", zoomHandler);
+    // setup move drag: alt + click
+    canvas.on("mouse:down", function (opt) {
+      var evt = opt.e;
+      if (evt.altKey === true) {
+        this.isDragging = true;
+        this.selection = false;
+        this.lastPosX = evt.clientX;
+        this.lastPosY = evt.clientY;
+      }
+    });
+    canvas.on("mouse:move", function (opt) {
+      if (this.isDragging) {
+        var e = opt.e;
+        var vpt = this.viewportTransform;
+        vpt[4] += e.clientX - this.lastPosX;
+        vpt[5] += e.clientY - this.lastPosY;
+        this.requestRenderAll();
+        this.lastPosX = e.clientX;
+        this.lastPosY = e.clientY;
+      }
+    });
+    canvas.on("mouse:up", function (opt) {
+      // on mouse up we want to recalculate new interaction
+      // for all objects, so we call setViewportTransform
+      this.setViewportTransform(this.viewportTransform);
+      this.isDragging = false;
+      this.selection = true;
+    });
     let initialImg = this.initialImage.bind(this);
     this.setState({ canvas }, initialImg);
   }
