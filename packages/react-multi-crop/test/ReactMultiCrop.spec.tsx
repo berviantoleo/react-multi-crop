00dd1268448c320f665b77f76be1d418bc189c3d
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-01-17 15:02:41 +0700

    fix: prepare testing template need more robust test

--- packages/react-multi-crop/test/ReactMultiCrop.spec.tsx
+++ packages/react-multi-crop/test/ReactMultiCrop.spec.tsx
@@ -20,3 +20,67 @@ test('show label', async () => {
   const textLabel = screen.getByText('React Multi Crop');
   expect(textLabel).toBeTruthy();
 });
+
+test('readonly mode', async () => {
+  render(<ReactMultiCrop readonly />);
+  const buttons = screen.queryByRole('button');
+  expect(buttons).toBeNull();
+});
+
+test('Initial crop', async () => {
+  render(
+    <ReactMultiCrop
+      readonly
+      image="https://picsum.photos/800"
+      width={800}
+      height={800}
+      record={{
+        clippings: [
+          {
+            id: 1,
+            rect: { x1: 0.0, y1: 0.0, x2: 0.2, y2: 0.2 },
+            rectPx: {},
+          },
+        ],
+      }}
+    />,
+  );
+  const buttons = screen.queryByRole('button');
+  expect(buttons).toBeNull();
+});
+
+test('Output Handler', async () => {
+  render(
+    <ReactMultiCrop
+      readonly
+      image="https://picsum.photos/800"
+      width={800}
+      height={800}
+      input={{
+        name: 'multicrop',
+        onChange: function (value: any) {
+          console.log(value);
+          const newObject = value[0];
+          if (newObject) {
+            console.log(newObject);
+            if (newObject.canvasElement) {
+              console.log(newObject.canvasElement.toDataURL());
+            }
+          }
+          setCropValue(value);
+        },
+      }}
+      record={{
+        clippings: [
+          {
+            id: 1,
+            rect: { x1: 0.0, y1: 0.0, x2: 0.2, y2: 0.2 },
+            rectPx: {},
+          },
+        ],
+      }}
+    />,
+  );
+  const buttons = screen.queryByRole('button');
+  expect(buttons).toBeNull();
+});
