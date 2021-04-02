297faf8108a2f16ba518c2b625132341f832f32f
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-04-02 09:08:12 +0700

    fix: update deps

--- packages/react-multi-crop/.storybook/main.js
+++ packages/react-multi-crop/.storybook/main.js
@@ -1,10 +1,7 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-docs/register",
-    "@storybook/addon-actions/register",
     "@storybook/addon-links/register",
-    "@storybook/addon-controls",
     "@storybook/addon-storysource/register",
     "@storybook/addon-essentials",
   ],
