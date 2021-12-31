74e1461d17561d6f53915cb50f79117a8bc5ac47
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-12-31 22:42:33 +0700

    fix: fix example failed build

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,7 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
   },
 };
