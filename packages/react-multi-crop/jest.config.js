b876ff95a07053fd5ce2e02eeb3ef6a8b8bb5032
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-05-29 22:45:11 +0700

    feat: update deps and update jest config

--- packages/react-multi-crop/jest.config.js
+++ packages/react-multi-crop/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/bower_components/', '<rootDir>/node_modules/'],
 };
