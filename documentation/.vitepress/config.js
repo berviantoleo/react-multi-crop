fb643cb19e8e8b48995e25d677ffac6421a4c432
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-02-24 18:00:32 +0700

    fix(docs): Migrate to vitepress (#1707)
    * Migrate to vitepress
    
    * Update output
    
    * Add the build docs to PR
    
    * Fix storybook to use webpack5

---
+++ documentation/.vitepress/config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+    title: "React Multi Crop",
+    description: "React Multi Crop for easy crop",
+    lang: "en-US",
+    base: process.env.BASE_URL || "/",
+    lastUpdated: true,
+    outDir: "../docs"
+});
