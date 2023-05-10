77ecb2ce72385056b7ad90066c1070074ee1b213
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-05-10 15:46:19 +0700

    Update preview

--- packages/react-multi-crop/.storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-storysource", "@storybook/addon-essentials"],
-  framework: "@storybook/react-webpack5",
-  docs: {
-    autodocs: true
-  }
-};
--- packages/react-multi-crop/.storybook/preview.js
+++
@@ -1,6 +0,0 @@
-import { configure } from "@storybook/react";
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-};
-configure(require.context("../src", true, /\.stories\.(js|ts|jsx|tsx)$/), module);
