8c32615b2f54ab9ec7325ef78b670aa4d92e36db
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-05-10 15:52:25 +0700

    fix: init update storybook (#1824)
    * fix: init update storybook
    
    * Update preview

---
+++ packages/react-multi-crop/.storybook/main.ts
@@ -0,0 +1,16 @@
+import { StorybookConfig } from "@storybook/types";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-storysource",
+    "@storybook/addon-essentials"
+  ],
+  framework: "@storybook/react-webpack5",
+  docs: {
+    autodocs: true
+  }
+};
+
+export default config;
--- packages/react-multi-crop/src/ReactMultiCrop/ReactMultiCrop.stories.tsx
+++ packages/react-multi-crop/src/ReactMultiCrop/ReactMultiCrop.stories.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Meta, Story } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import ReactMultiCrop from './ReactMultiCrop';
-import { IOutputData, IReactMultiCropProps } from './interfaces';
+import { IOutputData } from './interfaces';
 
 export default {
-  title: 'React Multi Crop',
   component: ReactMultiCrop,
   argTypes: {
     cropBackgroundColor: {
@@ -14,126 +13,124 @@ export default {
       },
     },
   },
-} as Meta;
+} as Meta<typeof ReactMultiCrop>;
 
-const Template: Story<IReactMultiCropProps> = (args: IReactMultiCropProps) => (
-  <ReactMultiCrop {...args} />
-);
+type Story = StoryObj<typeof ReactMultiCrop>;
 
-export const DefaultEditable = Template.bind({});
-
-DefaultEditable.args = {
-  id: 'defaultEdit',
-  image: 'https://picsum.photos/800',
-  readonly: false,
-  showButton: false,
-  includeDataUrl: false,
-  includeHtmlCanvas: false,
-  cropBackgroundColor: 'yellow',
-  cropBackgroundOpacity: 0.3,
-  input: {
-    onChange(value: Array<IOutputData>) {
-      console.log(value);
+export const DefaultEditable: Story = {
+  args: {
+    id: 'defaultEdit',
+    image: 'https://picsum.photos/800',
+    readonly: false,
+    showButton: false,
+    includeDataUrl: false,
+    includeHtmlCanvas: false,
+    cropBackgroundColor: 'yellow',
+    cropBackgroundOpacity: 0.3,
+    input: {
+      onChange(value: Array<IOutputData>) {
+        console.log(value);
+      },
     },
   },
 };
 
-export const DefaultReadOnly = Template.bind({});
-
-DefaultReadOnly.args = {
-  id: 'defaultReadonly',
-  image: 'https://picsum.photos/800',
-  readonly: true,
-  showButton: false,
-  includeDataUrl: false,
-  includeHtmlCanvas: false,
-  cropBackgroundColor: 'yellow',
-  cropBackgroundOpacity: 0.3,
-  record: {
-    clippings: [
-      {
-        id: '1',
-        rect: {
-          x1: 0.1,
-          x2: 0.2,
-          y1: 0.1,
-          y2: 0.2,
+export const DefaultReadOnly: Story = {
+  args: {
+    id: 'defaultReadonly',
+    image: 'https://picsum.photos/800',
+    readonly: true,
+    showButton: false,
+    includeDataUrl: false,
+    includeHtmlCanvas: false,
+    cropBackgroundColor: 'yellow',
+    cropBackgroundOpacity: 0.3,
+    record: {
+      clippings: [
+        {
+          id: '1',
+          rect: {
+            x1: 0.1,
+            x2: 0.2,
+            y1: 0.1,
+            y2: 0.2,
+          },
         },
-      },
-      {
-        id: '2',
-        rect: {
-          x1: 0.3,
-          x2: 0.4,
-          y1: 0.3,
-          y2: 0.4,
+        {
+          id: '2',
+          rect: {
+            x1: 0.3,
+            x2: 0.4,
+            y1: 0.3,
+            y2: 0.4,
+          },
         },
-      },
-      {
-        id: '3',
-        rect: {
-          x1: 0.8,
-          x2: 1.0,
-          y1: 0.8,
-          y2: 1.0,
+        {
+          id: '3',
+          rect: {
+            x1: 0.8,
+            x2: 1.0,
+            y1: 0.8,
+            y2: 1.0,
+          },
         },
+      ],
+    },
+    input: {
+      onChange(value: Array<IOutputData>) {
+        console.log(value);
       },
-    ],
-  },
-  input: {
-    onChange(value: Array<IOutputData>) {
-      console.log(value);
     },
   },
 };
 
-export const ShowButton = Template.bind({});
-
-ShowButton.args = {
-  id: 'showButton',
-  image: 'https://picsum.photos/800',
-  readonly: false,
-  showButton: true,
-  addButton: <button>Add</button>,
-  deleteButton: <button>Delete</button>,
-  includeDataUrl: false,
-  includeHtmlCanvas: false,
-  cropBackgroundColor: 'yellow',
-  cropBackgroundOpacity: 0.3,
-  record: {
-    clippings: [
-      {
-        id: '1',
-        rect: {
-          x1: 0.1,
-          x2: 0.2,
-          y1: 0.1,
-          y2: 0.2,
+export const ShowButton: Story = {
+  args: {
+    id: 'showButton',
+    image: 'https://picsum.photos/800',
+    readonly: false,
+    showButton: true,
+    addButton: <button>Add</button>,
+    deleteButton: <button>Delete</button>,
+    includeDataUrl: false,
+    includeHtmlCanvas: false,
+    cropBackgroundColor: 'yellow',
+    cropBackgroundOpacity: 0.3,
+    record: {
+      clippings: [
+        {
+          id: '1',
+          rect: {
+            x1: 0.1,
+            x2: 0.2,
+            y1: 0.1,
+            y2: 0.2,
+          },
         },
-      },
-      {
-        id: '2',
-        rect: {
-          x1: 0.3,
-          x2: 0.4,
-          y1: 0.3,
-          y2: 0.4,
+        {
+          id: '2',
+          rect: {
+            x1: 0.3,
+            x2: 0.4,
+            y1: 0.3,
+            y2: 0.4,
+          },
         },
-      },
-      {
-        id: '3',
-        rect: {
-          x1: 0.8,
-          x2: 1.0,
-          y1: 0.8,
-          y2: 1.0,
+        {
+          id: '3',
+          rect: {
+            x1: 0.8,
+            x2: 1.0,
+            y1: 0.8,
+            y2: 1.0,
+          },
         },
+      ],
+    },
+    input: {
+      onChange(value: Array<IOutputData>) {
+        console.log(value);
       },
-    ],
-  },
-  input: {
-    onChange(value: Array<IOutputData>) {
-      console.log(value);
     },
   },
 };
