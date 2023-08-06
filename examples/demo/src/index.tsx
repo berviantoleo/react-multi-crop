1d43317f795cb2d3bfb44d4711dc4b0d5af04733
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-08-06 20:47:33 +0700

    fix: fix linting

--- examples/demo/src/index.tsx
+++ examples/demo/src/index.tsx
@@ -1,4 +1,4 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
