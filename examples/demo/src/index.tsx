8347e080842888f98d088814dc544c1483c376d7
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-08-06 21:21:07 +0700

    feat: update deps (#2065)
    * feat: update deps
    
    * fix: use yarn 3
    
    * fix: update deprecated
    
    * fix: fix test deprecated
    
    * fix: fix linting
    
    * fix: fix null
    
    * fix: removc component temporary

--- examples/demo/src/index.tsx
+++ examples/demo/src/index.tsx
@@ -1,11 +1,14 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 const rootElement = document.getElementById("root");
-// eslint-disable-next-line
-ReactDOM.render(<App />, rootElement);
+if (rootElement) {
+  const root = createRoot(rootElement);
+  // eslint-disable-next-line
+    root.render(<App />);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
