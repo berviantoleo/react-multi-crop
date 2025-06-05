2c459f989f0df7ddbc4b484e453c10df1d92e48a
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-06-05 09:41:36 +0000

    fix: remove config

--- packages/react-multi-crop/.storybook/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.(stories|story)\.(jsx|tsx)?$/,
-    exclude: [/node_modules/],
-    enforce: 'pre',
-  });
-
-  return config;
-};
