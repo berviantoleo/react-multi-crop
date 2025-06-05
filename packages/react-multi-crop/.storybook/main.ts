914cf47efc3dd08b5e3a3befeae50a7e9aef720b
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-06-05 09:36:04 +0000

    fix: remove addons

--- packages/react-multi-crop/.storybook/main.ts
+++ packages/react-multi-crop/.storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-a11y',
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@storybook/addon-webpack5-compiler-babel',
   ],
   framework: '@storybook/react-webpack5',
